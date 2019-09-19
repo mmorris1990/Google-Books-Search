c35c57a9684e871140f080a5b059d7af1abeb1de
Michael Morris <mmorris1990@gmail.com>
2019-09-19 13:56:31 -0500

    adjusted routing, built comtrollers

--- models/book.js
+++ models/book.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true },
     authors: { type: String, required: true },
-    description: String,
-    image: String,
-    link: String,
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    link: { type: String, required: true },
     date: { type: Date, default: Date.now }
 });
 
